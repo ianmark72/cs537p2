9fb143ebf34bb3faa930a785fd3cae07658fc34e
Ian Mark <imark@royal-13.cs.wisc.edu>
2018-10-13 19:25:50 -0500

    changes

--- reader.c
+++ reader.c
@@ -1,7 +1,16 @@
+#include <reader.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 
-int func readFile(char *filename){
-	
+int func read() {
+	char* buffer = NULL;
+	buffer = calloc(100, sizeof(char));
+	//replace with BUF_SIZE
+	for( int i = 0; i < 100; i++ ){
+		
+		getc(buffer);
+		if (buffer[i] == '\n') { //add line to queue }
+	}
+	printf("%s", buffer);
 }
---
+++ reader.h
@@ -0,0 +1,7 @@
+#ifndef READER_H
+#define READER_H
+
+int read();
+
+#endif /* READER_H */
+
