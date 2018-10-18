c83dce8448c4eedf68213bbd91d6853315cfdda6
Ian Mark <imark@royal-13.cs.wisc.edu>
2018-10-18 14:45:18 -0500

    test file

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "reader.h"
+#include "munch1.h"
+#include "munch2.h"
+#include "writer.h"
+#include "queue.h"
+
+int main() {
+
+    read(NULL);
+
+    return 0;
+}
+
