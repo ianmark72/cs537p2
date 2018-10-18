8d066a15b2bf9f6d5744a3cf080d53db47181b6c
Ian Mark <imark@royal-12.cs.wisc.edu>
2018-10-18 01:30:38 -0500

    reader fix

--- reader.c
+++ reader.c
@@ -2,33 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "queue.h"
+//#include "queue.h"
 
 void* read(void* q) {
-	Queue* queue = (Queue*) q;
+	//Queue* queue = (Queue*) q;
 	const int BUF_SIZE = 1024;
 	char* string;
-	
+// commented out enque for testing	
 	char input = getchar();
 	if ( input != '\0' && input != EOF ) {
 		char* buffer = (char*)calloc(BUF_SIZE, sizeof(char));
 		int ctr = 0;
 		while ( input != EOF && input != '\0') {
 			if ( ctr > BUF_SIZE - 1) { 
-				//send error to std
+				fprintf(stderr, "exceeded buffer size");
 			}
 			if ( input != '\n') {
 				buffer[ctr] = input;
 				input = getchar();
 				ctr++;
 			} else {
-				string = (char*)calloc(strlen(buffer), sizeof(char));
+				buffer[ctr] = input;
+				printf("about to calloc string\n");
+				string = (char*)calloc(BUF_SIZE, sizeof(char));
+				if(string == NULL) { fprintf(stderr, "error calloc");}
+				printf("%s", buffer);
 				strncpy(string, buffer, BUF_SIZE);
-				EnqueueString(queue, string);
-				memset(buffer, 0, strnlen(buffer, BUF_SIZE));
+				//EnqueueString(queue, string);
+				printf("%s", string);
+				memset(buffer, 0, BUF_SIZE);
+				printf("after memset: %s\n",buffer);
 				ctr = 0;
+				input = getchar();
 			}
 		}
+		free(buffer);
 	}
 	return NULL;
 }
