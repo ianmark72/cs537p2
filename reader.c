4f4edee5cf9b8530e5a8f652e2f67b4a6a230679
Ian Mark <imark@royal-13.cs.wisc.edu>
2018-10-18 14:53:14 -0500

    reader changes

--- reader.c
+++ reader.c
@@ -9,23 +9,27 @@ void* read(void* q) {
 	const int BUF_SIZE = 1024;
 	char* string;
 // commented out enque for testing	
+// add null pointer with EOF
 	char input = getchar();
 	if ( input != '\0' && input != EOF ) {
 		char* buffer = (char*)calloc(BUF_SIZE, sizeof(char));
 		int ctr = 0;
+		//reorganize loop condition 
 		while ( input != EOF && input != '\0') {
 			if ( ctr > BUF_SIZE - 1) { 
 				fprintf(stderr, "exceeded buffer size");
 			}
-			if ( input != '\n') {
+			if ( input != '\n' && input != EOF) {
 				buffer[ctr] = input;
 				input = getchar();
 				ctr++;
 			} else {
+				printf("here");
 				buffer[ctr] = input;
 				string = (char*)calloc(BUF_SIZE, sizeof(char));
 				if(string == NULL) { fprintf(stderr, "error calloc");}
 				strncpy(string, buffer, BUF_SIZE);
+				printf("%s",string);
 				//EnqueueString(queue, string);
 				memset(buffer, 0, BUF_SIZE);
 				ctr = 0;
